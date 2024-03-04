19f2a67b24ae0b3ee2fbddf9f34727816ac6d2b2
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2024-03-04 21:31:16 +0700

    fix: Correct code in v-buku.php

--- admin/v_buku.php
+++ admin/v_buku.php
@@ -6,19 +6,19 @@
                    ?>
                   <tr>
                     <td><?php echo $no++; ?></td>
-                    <td><?php echo $x['id_buku']; ?></td>
+                    <td><?php echo $x['idBuku']; ?></td>
                     <td><?php echo $x['kategori']; ?></td>
-                    <td><?php echo $x['nama_buku']; ?></td>
+                    <td><?php echo $x['namaBuku']; ?></td>
                     <td><?php echo $x['harga']; ?></td>
                     <td><?php echo $x['stok']; ?></td>
                     <td><?php echo $x['penerbit']; ?></td>
                    
                     <td>
-                        <a href="edit_buku.php?id_buku=<?php echo $x['id_buku']; ?>&aksi=edit">
+                        <a href="edit_buku.php?id_buku=<?php echo $x['idBuku']; ?>&aksi=edit">
                         <button type="button" class="btn btn-success">Edit</button></a>
                        
                         <a class="remove"
-                        href="../controller.php?action=delete&id_buku=<?php echo $x['id_buku']; ?>">
+                        href="../controller.php?action=delete&id_buku=<?php echo $x['idBuku']; ?>">
                         <button type="button" class="btn btn-danger">Hapus</button></a>
                        
                     </td>
