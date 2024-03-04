3f057b7697f2e0c1232a1375e8a215a29d7edcd1
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2024-03-04 21:26:28 +0700

    feat: Add controller.php file

---
+++ controller.php
@@ -0,0 +1,34 @@
+<?php
+include('koneksi.php');
+$koneksi = new database();
+
+
+$action = $_GET['action'];
+if($action == "add")
+{
+    $koneksi->tambah_data($_POST['id_buku'],$_POST['kategori'],$_POST['nama_buku'],$_POST['harga'],
+    $_POST['stok'],$_POST['penerbit']);
+    header('location:admin/v_buku.php');
+}
+elseif($action=="edit")
+{
+    $koneksi->update_data($_POST['id_buku'],$_POST['kategori'],$_POST['nama_buku'],$_POST['harga'],
+    $_POST['stok'],$_POST['penerbit']);
+    header('location:admin/v_buku.php');
+}
+
+
+elseif($action=="delete")
+{
+    $id_buku = $_GET['id_buku'];
+    $koneksi->delete_data($id_buku);
+    header('location:admin/v_buku.php');
+}
+elseif ($action=="login")
+{
+    $koneksi->cek_data($_POST['username'],$_POST['password']);
+   
+}
+
+
+?>
